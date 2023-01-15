40c2c6b3f9173b751f4bb9f8d8ce32443e2070b1
atharvb0210 <103303867+atharvb0210@users.noreply.github.com>
2023-01-15 18:24:44 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,5 @@
+document.getElementById("getstarted").addEventListener("click",redirectFunction);
+document.getElementById("log").addEventListener("click",redirectFunction);
+function redirectFunction(){
+    window.location.href = "http://127.0.0.1:5500/page2/signup.html";
+}
